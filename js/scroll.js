42cd910f399d56a1a9dd90e644d4087e205999d7
Luckyman <iyedtouato@gmail.Com>
2025-06-02 23:28:21 +0100

    initial gfggv

--- js/scroll.js
+++ js/scroll.js
@@ -1,48 +1,57 @@
+window.addEventListener('scroll', function () {
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn')
+  if (window.pageYOffset > 300) {
+    // Show after 300px scroll
+    scrollToTopBtn.classList.add('show')
+  } else {
+    scrollToTopBtn.classList.remove('show')
+  }
+})
 
 function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth' // Smooth scrolling effect
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth', // Smooth scrolling effect
+  })
 }
 // Get the modal
 function navigateToPage() {
-    window.location.href = 'jci_app.html'; // Change 'other-page.html' to your target page
+  window.location.href = 'jci_app.html' // Change 'other-page.html' to your target page
 }
 function navigateToHealthPage() {
-    window.location.href = 'health_app.html'; // Change 'other-page.html' to your target page
+  window.location.href = 'health_app.html' // Change 'other-page.html' to your target page
 }
 function navigateToKarhatyPage() {
-    window.location.href = 'karhabti.app.html'; // Change 'other-page.html' to your target page
+  window.location.href = 'karhabti.app.html' // Change 'other-page.html' to your target page
 }
 function downloadStaticFile(filePath, fileName) {
-    // Create an anchor element
-    const link = document.createElement('a');
-    
-    // Set the href attribute to the relative path of the file
-    link.href = filePath;
-    
-    // Set the download attribute to specify the file name
-    link.download = fileName;
-    
-    // Append the anchor to the body (needed for Firefox)
-    document.body.appendChild(link);
-    
-    // Programmatically click the link to trigger the download
-    link.click();
-    
-    // Remove the link from the DOM
-    document.body.removeChild(link);
+  // Create an anchor element
+  const link = document.createElement('a')
+
+  // Set the href attribute to the relative path of the file
+  link.href = filePath
+
+  // Set the download attribute to specify the file name
+  link.download = fileName
+
+  // Append the anchor to the body (needed for Firefox)
+  document.body.appendChild(link)
+
+  // Programmatically click the link to trigger the download
+  link.click()
+
+  // Remove the link from the DOM
+  document.body.removeChild(link)
 }
 function openGmailCompose(to, subject, body) {
-    // Encode the parameters to ensure they are URL-safe
-    const encodedTo = encodeURIComponent(to);
-    const encodedSubject = encodeURIComponent(subject);
-    const encodedBody = encodeURIComponent(body);
+  // Encode the parameters to ensure they are URL-safe
+  const encodedTo = encodeURIComponent(to)
+  const encodedSubject = encodeURIComponent(subject)
+  const encodedBody = encodeURIComponent(body)
 
-    // Construct the Gmail compose URL
-    const gmailComposeUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodedTo}&su=${encodedSubject}&body=${encodedBody}`;
+  // Construct the Gmail compose URL
+  const gmailComposeUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodedTo}&su=${encodedSubject}&body=${encodedBody}`
 
-    // Open the URL in a new tab
-    window.open(gmailComposeUrl, '_blank');
+  // Open the URL in a new tab
+  window.open(gmailComposeUrl, '_blank')
 }
