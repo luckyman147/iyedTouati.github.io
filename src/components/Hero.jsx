85b373be87957855133bb475f5dbf13cd7c9e4b0
Luckyman <iyedtouato@gmail.Com>
2026-04-09 22:33:31 +0100

    changinging layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ export default function Hero({ data, socialData }) {
           />
         </div>
         
-        <div className="flex-1 flex items-center justify-center">
+        <div className="flex-[1.4] flex items-center justify-center">
           <motion.div
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
